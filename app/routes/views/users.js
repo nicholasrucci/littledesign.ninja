8a07600a4f3171d2414d3c2d8783b9b31cf9403b
Nick <ld.tech@outlook.com>
2015-11-13 11:54:09 -0700

    User sign up implementation

---
+++ app/routes/views/users.js
@@ -0,0 +1,10 @@
+(function(){
+	'use strict';
+
+	var UsersController = require('../../controllers/users.js');
+
+	module.exports = function(app) {
+		app.get('/users/new', UsersController.new);
+		app.post('/users/create', UsersController.create);
+	};
+})();
